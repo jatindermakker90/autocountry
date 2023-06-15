48d6ba0d064c949191aa282e6969d98740b5609b
Lenovo Gagan <cu.gagankashyap@gmail.com>
2023-06-15 23:28:54 +0530

    changes

--- app/Imports/TiresDataImport.php
+++ app/Imports/TiresDataImport.php
@@ -26,30 +26,38 @@ class TiresDataImport implements ToModel,WithStartRow
            // Login for remove discarded sku from uploaded CSV
            $checkIfSkuExists = DiscardedItems::where('item_code', '=', $row[2])->where('category','tires')->first();
            if(empty($checkIfSkuExists)){
-            return new TiresData([
-                'brand_name' => $row[0], 'model' => $row[1], 'sku_code' => $row[2],
-                'size' => $row[3], 'load_range' => $row[4], 'speed_rating' => $row[5],
-                'load_description' => $row[6], 'description' => $row[7], 'price' => $row[8],
-                'quantity' => $row[9], 'season' => $row[10], 'winter_approved' => $row[11],
-                'studdable' => $row[12], 'image1' => $row[13], 'image2' => $row[14],
-                'image3' => $row[15], 'image4' => $row[16], 'image5' => $row[17],
-                'image6' => $row[18],
-            ]);
+             if(str_contains($row[1], '868')){
+               //
+             } else {
+               return new TiresData([
+                   'brand_name' => $row[0], 'model' => $row[1], 'sku_code' => $row[2],
+                   'size' => $row[3], 'load_range' => $row[4], 'speed_rating' => $row[5],
+                   'load_description' => $row[6], 'description' => $row[7], 'price' => $row[8],
+                   'quantity' => $row[9], 'season' => $row[10], 'winter_approved' => $row[11],
+                   'studdable' => $row[12], 'image1' => $row[13], 'image2' => $row[14],
+                   'image3' => $row[15], 'image4' => $row[16], 'image5' => $row[17],
+                   'image6' => $row[18],
+               ]);
+             }
           }
       } else if($row[0] == 'GREENTRAC'||$row[0] == 'HAIDA' || $row[0] == 'LIONHART'
                 || $row[0] == 'VENOM'|| $row[0] == 'VERSA') {
           // Login for remove discarded sku from uploaded CSV
           $checkIfSkuExists = DiscardedItems::where('item_code', '=', $row[2])->where('category','tires')->first();
           if(empty($checkIfSkuExists)){
-            return new TiresData([
-                'brand_name' => $row[0], 'model' => $row[1], 'sku_code' => $row[2],
-                'size' => $row[3], 'load_range' => $row[4], 'speed_rating' => $row[5],
-                'load_description' => $row[6], 'description' => $row[7], 'price' => $row[8],
-                'quantity' => $row[9], 'season' => $row[10], 'winter_approved' => $row[11],
-                'studdable' => $row[12], 'image1' => $row[13], 'image2' => $row[14],
-                'image3' => $row[15], 'image4' => $row[16], 'image5' => $row[17],
-                'image6' => $row[18],
-            ]);
+            if(str_contains($row[1], '868')){
+              //
+            } else {
+              return new TiresData([
+                  'brand_name' => $row[0], 'model' => $row[1], 'sku_code' => $row[2],
+                  'size' => $row[3], 'load_range' => $row[4], 'speed_rating' => $row[5],
+                  'load_description' => $row[6], 'description' => $row[7], 'price' => $row[8],
+                  'quantity' => $row[9], 'season' => $row[10], 'winter_approved' => $row[11],
+                  'studdable' => $row[12], 'image1' => $row[13], 'image2' => $row[14],
+                  'image3' => $row[15], 'image4' => $row[16], 'image5' => $row[17],
+                  'image6' => $row[18],
+              ]);
+            }
           }
       }
     }
--- app/Imports/WheelsDataImport.php
+++ app/Imports/WheelsDataImport.php
@@ -25,22 +25,9 @@ class WheelsDataImport implements ToModel,WithStartRow
          // Login for remove discarded sku from uploaded CSV
          $checkIfSkuExists = DiscardedItems::where('item_code', '=', $row[2])->where('category','wheels')->first();
          if(empty($checkIfSkuExists)){
-            return new WheelsData([
-                'brand_name' => $row[0], 'model' => $row[1], 'sku_code' => $row[2],
-                'finish' => $row[3], 'size' => $row[5].'x'.$row[6], 'diameter' => $row[5],
-                'width' => $row[6], 'bolt_pattern' => $row[7], 'offset' => $row[8],
-                'backspace' => $row[9], 'bore' => $row[10], 'price' => $row[11],
-                'quantity' => $row[12], 'image1' => $row[13], 'image2' => $row[14],
-                'image3' => $row[15], 'image4' => $row[16], 'image5' => $row[17],
-                'image6' => $row[18],
-            ]);
-       }
-      } else if($row[0] == 'Moto Metal'||$row[0] == 'DIM' || $row[0] == 'Fuel'
-                || $row[0] == 'GT OFFROAD'|| $row[0] == 'Sentali Forged' || $row[0] == 'Sentali Barrel Forged'
-                || $row[0] == 'Sentali Street'|| $row[0] == 'XF OFFROAD') {
-          // Login for remove discarded sku from uploaded CSV
-          $checkIfSkuExists = DiscardedItems::where('item_code', '=', $row[2])->where('category','wheels')->first();
-          if(empty($checkIfSkuExists)){
+           if(str_contains($row[1], 'XFX')){
+             //
+           } else {
              return new WheelsData([
                  'brand_name' => $row[0], 'model' => $row[1], 'sku_code' => $row[2],
                  'finish' => $row[3], 'size' => $row[5].'x'.$row[6], 'diameter' => $row[5],
@@ -49,7 +36,28 @@ class WheelsDataImport implements ToModel,WithStartRow
                  'quantity' => $row[12], 'image1' => $row[13], 'image2' => $row[14],
                  'image3' => $row[15], 'image4' => $row[16], 'image5' => $row[17],
                  'image6' => $row[18],
-             ]);
+             ]); 
+           }
+       }
+      } else if($row[0] == 'Moto Metal'||$row[0] == 'DIM' || $row[0] == 'Fuel'
+                || $row[0] == 'GT OFFROAD'|| $row[0] == 'Sentali Forged' || $row[0] == 'Sentali Barrel Forged'
+                || $row[0] == 'Sentali Street'|| $row[0] == 'XF OFFROAD') {
+          // Login for remove discarded sku from uploaded CSV
+          $checkIfSkuExists = DiscardedItems::where('item_code', '=', $row[2])->where('category','wheels')->first();
+          if(empty($checkIfSkuExists)){
+            if(str_contains($row[1], 'XFX')){
+              //
+            } else {
+              return new WheelsData([
+                  'brand_name' => $row[0], 'model' => $row[1], 'sku_code' => $row[2],
+                  'finish' => $row[3], 'size' => $row[5].'x'.$row[6], 'diameter' => $row[5],
+                  'width' => $row[6], 'bolt_pattern' => $row[7], 'offset' => $row[8],
+                  'backspace' => $row[9], 'bore' => $row[10], 'price' => $row[11],
+                  'quantity' => $row[12], 'image1' => $row[13], 'image2' => $row[14],
+                  'image3' => $row[15], 'image4' => $row[16], 'image5' => $row[17],
+                  'image6' => $row[18],
+              ]);
+            }
         }
       }
     }
